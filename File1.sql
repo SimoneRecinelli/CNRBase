b47d5aa999b2f823b20f99abaa6782ff51551a46
Paccise <mazzada2001@gmail.com>
2022-12-30 17:06:18 +0100

    inserimento animali pescati e drop table

--- File1.sql
+++ File1.sql
@@ -151,39 +151,6 @@ insert into Irbim (SedeAmm, ParIVA, Figura) values ('Ancona', 21145647324, 'Ammi
 insert into Irbim (SedeAmm, ParIVA, Figura) values ('Mazara Del Vallo', 34528374621, 'Amministratore');
 insert into Irbim (SedeAmm, ParIVA, Figura) values ('Lesina', 98726327436, 'Amministratore');
 
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (00728, 'Scampo', 'C' , 'M', 000.18, 00.078, '1A');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (00352, 'Scampo', 'N' , 'F', 000.12, 00.069, '3A');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (00096, 'Scampo', 'N' , 'F', 000.11, 00.066, '2B');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (01233, 'Scampo', 'C' , 'M', 000.20, 00.085, '3A');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (06991, 'Scampo', 'N' , 'F', 000.14, 00.072, '1B');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (00094, 'Merluzzo', 'C' , 'M', 000.86, 13.660, '4A');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (00522, 'Merluzzo', 'N' , 'M', 000.97, 17.320, '2B');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (03851, 'Merluzzo', 'C' , 'F', 001.04, 18.240, '1A');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (09427, 'Merluzzo', 'C' , 'F', 000.92, 15.800, '2A');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (00447, 'Merluzzo', 'N' , 'M', 001.16, 20.450, '2B');
-
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (00853, 'Merluzzo', 'C' , 'M', 001.23, 22.380, '1B');
-
 insert into Fattura (NumOrd, CostoTot, QBarche, Cedente, Cessionario) values (1589,1502.00, 10,'CASSA DI RISPARMIO DI FERMO','CNR ISTITUTO RISORSE BIOLOGICHE');
 insert into Fattura (NumOrd, CostoTot, QBarche, Cedente, Cessionario) values (1385,2300.00, 15,'CASSA DI RISPARMIO DI OSIMO','CNR ISTITUTO RISORSE BIOLOGICHE');
 insert into Fattura (NumOrd, CostoTot, QBarche, Cedente, Cessionario) values (4857,1260.00, 8,'CASSA DI RISPARMIO DI MACERATA','CNR ISTITUTO RISORSE BIOLOGICHE');
@@ -192,18 +159,34 @@ insert into Fattura (NumOrd, CostoTot, QBarche, Cedente, Cessionario) values (43
 insert into Fattura (NumOrd, CostoTot, QBarche, Cedente, Cessionario) values (509,1198.00, 7,'CASSA DI RISPARMIO DI FERMO','CNR ISTITUTO RISORSE BIOLOGICHE');
 insert into Fattura (NumOrd, CostoTot, QBarche, Cedente, Cessionario) values (102,5400.00, 35,'CASSA DI RISPARMIO DI OSIMO','CNR ISTITUTO RISORSE BIOLOGICHE');
 insert into Fattura (NumOrd, CostoTot, QBarche, Cedente, Cessionario) values (8743,3489.00, 23,'CASSA DI RISPARMIO DI MACERATA','CNR ISTITUTO RISORSE BIOLOGICHE');
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (03750, 'Vongola', 'C' , 'M', 000.20, 00.025, '2A');
 
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (02450, 'Vongola', 'C' , 'M', 000.22, 00.022, '2B');
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (00728, 'Scampo', 'C' , 'M', 000.18, 00.078, '1A');
 
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (02950, 'Vongola', 'C' , 'M', 000.17, 00.024, '3A');
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)values (00352, 'Scampo', 'N' , 'F', 000.12, 00.069, '3A');
 
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (03970, 'Vongola', 'S' , 'M', 000.23, 00.029, '1A');
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (00096, 'Scampo', 'N' , 'F', 000.11, 00.066, '2B');
 
-insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
-values (04650, 'Vongola', 'C' , 'M', 000.21, 00.027, '3B');
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (01233, 'Scampo', 'C' , 'M', 000.20, 00.085, '3A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (06991, 'Scampo', 'N' , 'F', 000.14, 00.072, '1B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (00094, 'Merluzzo', 'C' , 'M', 000.86, 13.660, '4A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (00522, 'Merluzzo', 'N' , 'M', 000.97, 17.320, '2B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (03851, 'Merluzzo', 'C' , 'F', 001.04, 18.240, '1A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (09427, 'Merluzzo', 'C' , 'F', 000.92, 15.800, '2A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (00447, 'Merluzzo', 'N' , 'M', 001.16, 20.450, '2B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (03750, 'Vongola', 'C' , 'M', 000.20, 00.025, '2A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (02450, 'Vongola', 'C' , 'M', 000.22, 00.022, '2B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (02950, 'Vongola', 'C' , 'M', 000.17, 00.024, '3A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (03970, 'Vongola', 'S' , 'M', 000.23, 00.029, '1A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat) values (04650, 'Vongola', 'C' , 'M', 000.21, 00.027, '3B');
 
