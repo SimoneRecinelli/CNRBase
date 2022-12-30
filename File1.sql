dd95a5864b0fd2966700ed989ad5fc344d6c7ef2
Paccise <mazzada2001@gmail.com>
2022-12-30 16:53:25 +0100

    inserimento animali pescati e drop table

--- File1.sql
+++ File1.sql
@@ -4,6 +4,7 @@ use cnrbase;
      ParIVA char(11) not null,
      Figura varchar(30)
      );
+
 create table Fattura (
     NumOrd integer primary key,
     CostoTot numeric(8,2),
@@ -60,6 +61,8 @@ create table Possesso(
      CodFisc char(16),
      primary key(IdBarca,CodFisc)
 );
+drop table Possesso;
+
 create table OperazioneDiPesca(
     IdOp integer primary key,
     GSA smallint,
@@ -71,16 +74,21 @@ create table OperazioneDiPesca(
     Qbarche smallint,
     data date
 );
+drop table OperazioneDiPesca;
+
 create table Utilizzo(
     IdBarca integer,
     IdOp integer,
     primary key(IdBarca,IdOp)
 );
+drop table Utilizzo;
+
 create table Cattura(
     IdOp integer,
     IdPesce integer,
     primary key(IdOp,IdPesce)
 );
+drop table Cattura;
 
 create table Imbarcazione (
     IdBarca integer primary key,
@@ -89,16 +97,19 @@ create table Imbarcazione (
     PortoP varchar(20),
     PortoA varchar(20)
 );
+drop table Imbarcazione;
 
 create table UscitaPescatore (
     CodFisc char(16) primary key,
     IdOp integer not null
 );
+drop table UscitaPescatore;
 
 create table UscitaRicercatore (
     CodFisc char(16) primary key,
     IdOp integer not null
 );
+drop table UscitaRicercatore;
 
 create table AnimalePescato (
     IdPesce integer primary key,
@@ -106,12 +117,57 @@ create table AnimalePescato (
     CatComm varchar(20),
     Sesso varchar(1),
     Lunghezza decimal (5,2),
-    Peso decimal (4,2),
-    StadioMat varchar(20),
-    Quantità decimal(8,2)
+    Peso decimal (5,3),
+    StadioMat varchar(2),
 );
-
+drop table AnimalePescato;
 show tables;
 
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00728, 'Scampo', 'C' , 'M', 000.18, 00.078, '1A');
 
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00352, 'Scampo', 'N' , 'F', 000.12, 00.069, '3A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00096, 'Scampo', 'N' , 'F', 000.11, 00.066, '2B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (01233, 'Scampo', 'C' , 'M', 000.20, 00.085, '3A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (06991, 'Scampo', 'N' , 'F', 000.14, 00.072, '1B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00094, 'Merluzzo', 'C' , 'M', 000.86, 13.660, '4A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00522, 'Merluzzo', 'N' , 'M', 000.97, 17.320, '2B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (03851, 'Merluzzo', 'C' , 'F', 001.04, 18.240, '1A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (09427, 'Merluzzo', 'C' , 'F', 000.92, 15.800, '2A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00447, 'Merluzzo', 'N' , 'M', 001.16, 20.450, '2B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00853, 'Merluzzo', 'C' , 'M', 001.23, 22.380, '1B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00550, 'Vongola', 'C' , 'M', 000.20, 00.025, '2A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00550, 'Vongola', 'C' , 'M', 000.22, 00.022, '2B');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00550, 'Vongola', 'C' , 'M', 000.17, 00.024, '3A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00550, 'Vongola', 'S' , 'M', 000.23, 00.029, '1A');
+
+insert into AnimalePescato(IdPesce,Nome,CatComm,Sesso,Lunghezza,Peso,StadioMat)
+values (00550, 'Vongola', 'C' , 'M', 000.21, 00.027, '3B');
 
